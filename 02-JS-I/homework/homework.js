d815b7885160858b10ca9e0a63296650e4b9590f
unknown <jairovillota82@gmail.com>
2022-07-02 22:24:17 -0500

    test 15 aproved

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -54,8 +54,7 @@ function sonIguales(x, y) {
 }
 
 function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
+
   if (str1.length===str2.length){
     return true
   }else{
@@ -68,7 +67,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num<90){
+    return  true
+  } else{
+    return  false
+  }
 }
 
 function mayorQueCincuenta(num) {
