858ca72c279e505211ed28fa9e3224fbfe8bc971
unknown <jairovillota82@gmail.com>
2022-07-02 23:06:01 -0500

    18 aproved

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -64,9 +64,7 @@ function tienenMismaLongitud(str1, str2) {
 }
 
 function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
+
   if (num<90){
     return  true
   } else{
@@ -78,19 +76,28 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num>50){
+    return true
+  }else{
+    return false
+  }
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return  x % y
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num % 2===0){
+    return  true
+  }else{
+    return false
+  }
   
 }
 
@@ -98,7 +105,11 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if  (num % 2===1){
+    return  false
+  }else{
+    return  true
+  }
 }
 
 function elevarAlCuadrado(num) {
