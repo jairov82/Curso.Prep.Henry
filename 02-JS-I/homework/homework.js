982d68641eb2ae6f84ea76348c2abcecb19f0a7e
unknown <jairovillota82@gmail.com>
2022-07-03 23:45:31 -0500

    24 aproved

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -116,37 +116,39 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return  Math.pow(num,2)
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return  Math.pow(num,3)
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+return  Math.pow(num,exponent)
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return  Math.round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+ return Math.ceil(num) 
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+ return Math.random() 
 }
 
 function esPositivo(numero) {
@@ -154,7 +156,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if  (numero>0){
+    return  'Es positivo';
+  }else if (numero===0){
+    return false  
+  }else{
+    return  'Es negativo'
+  }
 }
 
 function agregarSimboloExclamacion(str) {
