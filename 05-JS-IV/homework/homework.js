22ea8cb1aa4ca5ed9bf45ca66d29832af019c60f
unknown <jairovillota82@gmail.com>
2022-07-10 22:40:18 -0500

    5 Approved JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto={
+    nombre: nombre,
+    edad: edad,
+    meow: function gato(){return 'Meow!'}
+
+  }
+    return objeto
 }
+
 
 
 function agregarPropiedad(objeto, property) {
@@ -14,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property]=null
+  return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,14 +31,18 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  
+  return objeto[metodo]()
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  
+  return objetoMisterioso.numeroMisterioso*5
 }
+
 
 function eliminarPropiedad(objeto, unaPropiedad) {
   // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad 
